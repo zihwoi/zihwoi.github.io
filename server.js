16f155b6d55acfbd69edc4f42c8113767499112d
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-01 13:36:24 +0800

    UPDATE structure and created server.js to run on localhost

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the 'assets' directory
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// Serve static files from the root folder and assets folder
+app.use(express.static(path.join(__dirname)));
 
+// Serve HTML files
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/projects.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'projects.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
