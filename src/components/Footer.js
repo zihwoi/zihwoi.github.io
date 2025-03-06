25474e75e2114890d76c48b45d47b84a5a449c64
unknown <72529512+zihwoi@users.noreply.github.com>
2025-03-06 22:42:54 +0800

    footer year change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="bg-warmBg shadow-md dark:bg-warmDarkBg">
       <div className="max-w-6xl mx-auto px-4 py-4">
         <div className="text-center text-warmText dark:text-warmLightText">
-          © 2024 Zenn. All rights reserved.
+          © 2025 Zenn. All rights reserved.
         </div>
       </div>
     </footer>
